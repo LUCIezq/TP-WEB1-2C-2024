a3ec86d6537493b8512eb65463c9a2d763113a70
Ezequiel Luci <lucieze02@icloud.com>
2024-10-21 22:47:48 -0300

    JS-Contador carrito en Home/Contador en tarjeta de curso

---
+++ Paginas/Cart-view/JS/script.js
@@ -0,0 +1,1 @@
+let contadorCarrito = sessionStorage.getItem('contadorCarrito');
--- Paginas/Inicio/JS/inicio.js
+++ Paginas/Inicio/JS/inicio.js
@@ -1,17 +1,56 @@
 const contador__carrito = document.getElementById('cart-count');
 const courses = document.querySelectorAll('.lesson__item');
-let count = 0;
+const lesson__item__buy = document.getElementById('lesson__button-buy');
+const container__button__buy = document.getElementById('container__button-buy');
+
+const button__buy__minus = document.getElementById('button_buy-minus');
+const button__buy__count = document.getElementById('button_buy-count');
+const button__buy__plus = document.getElementById('button_buy-plus');
 
 
+let contador = 0;
 
+button__buy__minus.addEventListener('click', e => {
 
-courses.forEach(course => {
+    if(contador>0){
+        contador--;
+        button__buy__count.textContent = contador;
+        actualizarTextContentContador(contador);
 
-    const button = course.querySelector('.lesson__item-button');
-
-    button.addEventListener('click', e => {
-        count++;
-        contador__carrito.textContent = count;
-    })
+    }
 
 })
+
+button__buy__plus.addEventListener('click', e => {
+    if(contador<10){
+        contador++;
+        button__buy__count.textContent = contador;
+        actualizarTextContentContador(contador);
+    }
+})
+
+lesson__item__buy.addEventListener('mouseover', e => {
+    lesson__item__buy.style.display = "none";
+    container__button__buy.style.display = "flex";
+})
+
+container__button__buy.addEventListener('mouseleave', e => {
+    lesson__item__buy.style.display = "block";
+    container__button__buy.style.display = "none";
+})
+
+
+
+function actualizarTextContentContador(contador) {
+    contador__carrito.textContent = contador;
+    sessionStorage.setItem('contadorCarrito', contador);
+}
+
+window.onload = function () {
+    contador= sessionStorage.getItem('contadorCarrito');
+
+    if (contador) {
+        contador__carrito.textContent = contador;
+        button__buy__count.textContent = contador;
+    }
+}
