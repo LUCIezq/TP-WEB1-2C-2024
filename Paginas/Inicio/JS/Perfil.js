f82289fb919d03ddf9c0c60ead276ae31a38d3bb
Ezequiel Luci <lucieze02@icloud.com>
2024-11-15 14:33:28 -0300

    JS-perfil_modificaciones

--- Paginas/Inicio/JS/Perfil.js
+++ Paginas/Inicio/JS/Perfil.js
@@ -1,23 +1,45 @@
-const perfil__icon = document.getElementById('perfil__icon');
-const perfil__content = document.getElementById('perfil__content');
+// const perfil__icon = document.getElementById('perfil__icon');
+// const perfil__content = document.getElementById('perfil__content');
 
+
+// perfil__icon.addEventListener('mouseover', () => {
+//     perfil__content.classList.add('dropdown__header--visible');
+// })
+
+// perfil__content.addEventListener('mouseleave', () => {
+//     perfil__content.classList.remove('dropdown__header--visible');
+
+// })
+
+// const buttons = document.getElementById('buttons');
+// const estaLogueado = JSON.parse(sessionStorage.getItem('estaLogueado'));
+
+// if (estaLogueado) {
+//     buttons.classList.add('buttons--hidden');
+//     perfil__icon.classList.add('dropdown__header--visible');
+// } else {
+//     buttons.classList.remove('buttons--hidden');
+//     perfil__icon.classList.remove('dropdown__header--visible');
+// }
+
+const buttons = document.getElementById('buttons');
+const perfil__container = document.getElementById('perfil__container');
+const perfil__icon = document.getElementById('perfil');
+const perfil__content = document.getElementById('perfil__content');
 
 perfil__icon.addEventListener('mouseover', () => {
     perfil__content.classList.add('dropdown__header--visible');
 })
-
 perfil__content.addEventListener('mouseleave', () => {
     perfil__content.classList.remove('dropdown__header--visible');
-
 })
 
-const buttons = document.getElementById('buttons');
-const estaLogueado = JSON.parse(sessionStorage.getItem('estaLogueado'));
+const estaLogueado = sessionStorage.getItem('estaLogueado');
 
 if (estaLogueado) {
     buttons.classList.add('buttons--hidden');
-    perfil__icon.classList.add('dropdown__header--visible');
+    perfil__container.classList.add('dropdown__header--visible');
 } else {
     buttons.classList.remove('buttons--hidden');
-    perfil__icon.classList.remove('dropdown__header--visible');
+    perfil__container.classList.remove('dropdown__header--visible');
 }
