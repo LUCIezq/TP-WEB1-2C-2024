4a82c62f2b3872fce61612e7bb20498aff75eb87
Ezequiel Luci <lucieze02@icloud.com>
2024-11-15 12:38:41 -0300

    JS-iniciar_sesion

--- Paginas/Sign-in/JS/Sign-in.js
+++ Paginas/Sign-in/JS/Sign-in.js
@@ -1,2 +1,33 @@
-const usuarios = JSON.parse(sessionStorage.getItem('usuarios'));
+const formulario = document.getElementById('form');
+const form__state = document.getElementById('form-state');
 
+
+function sePuedeIniciarSesion() {
+    const email = document.getElementById('email').value;
+    const contrasenia = document.getElementById('password').value;
+    const usuarios = JSON.parse(sessionStorage.getItem('usuarios'));
+
+
+    if (usuarios) {
+        const usuarioBuscado = usuarios.find(usuario => usuario.email === email);
+
+        if (usuarioBuscado && contrasenia === usuarioBuscado.contrasenia) {
+            return true;
+        }
+    }
+    form__state
+    return false;
+
+}
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (sePuedeIniciarSesion()) {
+        formulario.submit();
+    } else {
+        form__state.style.textAlign = ' center';
+        form__state.classList.add('form__state--activo');
+    }
+
+})
